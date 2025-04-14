affe2165af343cfc2e6cdf3a2d5e0ba406e2e8a1
Aleksandra <abaibik@gmail.com>
2025-04-14 14:44:58 +0200

    fix 2Aufgabe even/odd index

--- Labor03/2Aufgabe.c
+++ Labor03/2Aufgabe.c
@@ -3,14 +3,14 @@
 
 int main(int argc, char *argv[])
 // Prints a specified number of symbols based on user input.
-// For every even index the symbol is an asterisk ('*'), for odd index it is an undercore "_"
+// For every even index the symbol is an undercore "_", for odd index it is an asterisk ('*')
 
 {
     int userNumber;
 
     scanf("%d", &userNumber);
 
-    for (int i = 1; i <= userNumber; i++)
+    for (int i = 0; i < userNumber; i++)
     {
         if (i % 2 == 0)
 
