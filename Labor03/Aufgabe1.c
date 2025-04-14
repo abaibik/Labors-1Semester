b905a6464ff6927f7998da54e5d76e45033cd9d6
Aleksandra <abaibik@gmail.com>
2025-04-14 10:33:38 +0200

    add Labor3, Aufgabe 1

---
+++ Labor03/Aufgabe1.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+// Prints a specified number of asterisks ('*') based on user input
+
+{
+    int userNumber;
+
+    scanf("%d", &userNumber);
+
+    for (int i = 1; i <= userNumber; i++)
+    {
+        printf("*");
+    }
+
+    return 0;
+}
