5450f7c3a423d595a56d0685f56825a3f75a5820
Aleksandra <abaibik@gmail.com>
2025-04-20 19:29:21 +0200

    reconfiguration CMake

---
+++ Labor03/Aufgabe6.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+/*
+Using the formula: DegreesFahrenheit = (DegreesCelsius * 9)/5 + 32,
+displays the temperature in both Celsius and Fahrenheit in table form.
+
+The table shows all values from -20°C to 300°C and their conversion to °F.
+The output should be right-aligned.
+*/
+
+{
+    printf("Celsius Fahrenheit\n");
+
+    // Loop, von -20, jede Schritte + 20.
+    // use i in formel
+    for (int i = -20; i <= 300; i += 20)
+    {
+        int fahrenheit = (i * 9) / 5 + 32;
+        printf("%3d        %3d\n", i, fahrenheit);
+    }
+
+    return 0;
+}
