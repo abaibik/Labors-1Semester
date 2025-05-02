64db433e915f433e45ae20937dff3bd71ef9ce14
Aleksandra <abaibik@gmail.com>
2025-05-02 14:01:24 +0200

    add Aufgabe2 (not solved yet)

---
+++ Labor05/Aufgabe2.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char string[40];
+    int count = 0;
+
+    printf("%d Kleinbuchstaben", count);
+
+    return 0;
+}
