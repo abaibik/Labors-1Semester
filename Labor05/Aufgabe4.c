d768d41d0670188ff00a0341af3096a16dbf6ccd
Aleksandra <abaibik@gmail.com>
2025-05-05 17:20:58 +0200

    add Aufgabe4

---
+++ Labor05/Aufgabe4.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char vowel;
+    char str[41];
+
+    scanf(" %c %40s", &vowel, str);
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+            str[i] = vowel;
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
