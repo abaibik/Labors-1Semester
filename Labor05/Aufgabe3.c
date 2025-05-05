f5e6ecbb2ff7bf6195e827c8f3d8dce624b98d3c
Aleksandra <abaibik@gmail.com>
2025-05-05 17:06:36 +0200

    add Aufgabe3

---
+++ Labor05/Aufgabe3.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char string[40];
+
+    if (!fgets(string, sizeof(string), stdin))
+        return 0;
+
+    int count = 0;
+    for (int i = 0; string[i] != '\n'; i++)
+    {
+        if (string[i] == 'a' || string[i] == 'e' || string[i] == 'i' || string[i] == 'o' || string[i] == 'u' || string[i] == 'A' || string[i] == 'E' || string[i] == 'I' || string[i] == 'O' ||
+            string[i] == 'U')
+            count++;
+    }
+
+    printf("%d Vokale", count);
+
+    return 0;
+}
