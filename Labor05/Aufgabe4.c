a06c93f20c754c091c0a4e9d54d30785bc7f2fcc
Aleksandra <abaibik@gmail.com>
2025-05-05 19:08:34 +0200

    add Aufgabe4

--- Labor05/Aufgabe4.c
+++ Labor05/Aufgabe4.c
@@ -3,18 +3,22 @@
 
 int main(int argc, char *argv[])
 {
-    char vowel;
-    char str[41];
+    char string[41];
+    int shift = 'a' - 'A';  // 32 in ASCII
 
-    scanf(" %c %40s", &vowel, str);
+    if (!fgets(string, sizeof(string), stdin))
+        return 0;
 
-    for (int i = 0; str[i] != '\0'; i++)
+    for (int i = 0; string[i]; i++)
     {
-        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
-            str[i] = vowel;
+        if (string[i] >= 'a' && string[i] <= 'z')
+            string[i] -= shift;
+
+        else if (string[i] >= 'A' && string[i] <= 'Z')
+            string[i] += shift;
     }
 
-    printf("%s", str);
+    printf("%s", string);
 
     return 0;
 }
