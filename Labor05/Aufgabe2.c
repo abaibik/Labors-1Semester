e690e9312592e5a8f4b088c55afeeae2fa847786
Aleksandra <abaibik@gmail.com>
2025-05-05 16:25:41 +0200

    complete Aufgabe2

--- Labor05/Aufgabe2.c
+++ Labor05/Aufgabe2.c
@@ -4,7 +4,16 @@
 int main(int argc, char *argv[])
 {
     char string[40];
+
+    if (!fgets(string, sizeof(string), stdin))
+        return 0;
+
     int count = 0;
+    for (int i = 0; string[i] != '\n'; i++)
+    {
+        if (string[i] >= 'a' && string[i] <= 'z')
+            count++;
+    }
 
     printf("%d Kleinbuchstaben", count);
 
