329887c5b2d1f54d6bec10546ed505f479fd5ec1
Aleksandra <abaibik@gmail.com>
2025-05-08 17:34:44 +0200

    add Aufgabe5

---
+++ Labor05/Aufgabe5.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    char string[41];
+    int shift = 'a' - 'A';  // 32 in ASCII
+
+    if (!fgets(string, sizeof(string), stdin))
+        return 0;
+
+    for (int i = 0; string[i]; i++)
+    {
+        if (string[i] >= 'a' && string[i] <= 'z')
+            string[i] -= shift;
+
+        else if (string[i] >= 'A' && string[i] <= 'Z')
+            string[i] += shift;
+    }
+
+    printf("%s", string);
+    return 0;
+}
