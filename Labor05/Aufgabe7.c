c90786f3c9f22d6252763a966d663001c9b76b71
Aleksandra <abaibik@gmail.com>
2025-05-08 19:52:46 +0200

    add Aufgabe7

---
+++ Labor05/Aufgabe7.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    char string[100];
+
+    scanf("%s", string);
+
+    int stringLength = 0;
+    while (string[stringLength] != '\0')
+        stringLength++;
+
+    for (int i = stringLength - 1; i >= 0; i--)
+        printf("%c", string[i]);
+
+    return 0;
+}
