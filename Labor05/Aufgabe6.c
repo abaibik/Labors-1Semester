0b9c7a428f5ba26d5b55165542e8b111f2739990
Aleksandra <abaibik@gmail.com>
2025-05-08 18:54:17 +0200

    add Aufgabe6

---
+++ Labor05/Aufgabe6.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    char string[41];
+    int startIndex, endIndex;
+
+    if (scanf("%40s %d %d", string, &startIndex, &endIndex) != 3)
+        return 0;
+
+    int stringLength = 0;
+    for (int i = 0; string[i] != '\0'; i++)
+        stringLength += 1;
+    if (startIndex < 0 || startIndex > endIndex || endIndex >= stringLength)
+        return 0;
+
+    printf("%s %d-%d: ", string, startIndex, endIndex);
+
+    for (int i = startIndex; i <= endIndex; i++)
+        printf("%c", string[i]);
+
+    return 0;
+}
