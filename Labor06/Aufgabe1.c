716833ca32b35731736e31a163dda17b3862ecf3
Aleksandra <abaibik@gmail.com>
2025-05-11 12:17:48 +0200

    add Aufgabe1

---
+++ Labor06/Aufgabe1.c
@@ -0,0 +1,27 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+/*
+Calculate and return the minimum of a integer sequence.
+
+size_t is the unsigned integer type of the result of sizeof, offsetof etc., depending on the data model.
+It can store the maximum size of a theoretically possible object of any type (including array).
+*/
+
+int minimum(int *array, size_t array_length)
+{
+
+    int min = array[0];
+
+    for (int i = 1; i < array_length; i++)
+    {
+        min = array[i] < min ? array[i] : min;
+    }
+    return min;
+}
+
+int main()
+{
+    int numbers[] = {4, 6, 9, 5};
+    printf("%d", minimum(numbers, 4));
+}
